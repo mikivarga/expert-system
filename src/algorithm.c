2bf14581053c836fed8aa78132d657ced9f77ad9
mvarga1 <mykhailo.varga@globallogic.com>
2019-04-05 19:12:22 +0300

    Need fix add_to_solver

--- src/algorithm.c
+++ src/algorithm.c
@@ -12,40 +12,75 @@
 
 #include "expert_system.h"
 
-static void add_to_solver(t_expert *data) //перенести до main?
+static void add_to_solver(t_expert *data)
 {
     int i;
     int j;
+    int z;
 
     i = 0;
     while (data->facts[i])
     {
         data->solver[DATA][i] = data->facts[i];
-        data->solver[STATUS][i] = TRUE;
-        data->solver[FACTS][i] = FALSE;
+        data->solver[STATUS][i] = '+';//TRUE;
+        //ft_printf("data->solver[STATUS][i] = '+' %s", data->solver[STATUS]);
+        data->solver[FACTS][i] = '-';//;FALSE;
         i++;
     }
-    if ('\0' == data->solver[DATA][i])
+    i = 0;
+    while (data->queries[i])
     {
-        while (data->queries[i])
+        j = 0;
+        while (data->solver[DATA][j])
         {
-            data->solver[DATA][i] = data->facts[i];
-            data->solver[STATUS][i] = FALSE;
-            data->solver[FACTS][i] = FALSE;
-            i++;
+            if (data->solver[DATA][j] == data->queries[i])
+            {
+                break ;
+            }
+            j++;
         }
+        if ('\0' == data->solver[DATA][j])
+        {
+            data->solver[DATA][j] = data->queries[i];
+            data->solver[STATUS][j] = '-';//FALSE;
+            data->solver[FACTS][j] = '-';//FALSE;
+        }
+        i++;
     }
-    else
+    i = 0;
+    while (data->rules[i])
     {
-        //check duplicate letters
+        j = 0;
+        while (data->rules[i][j])
+        {
+            if (ALPHA(data->rules[i][j]))
+            {
+                z = 0;
+                while (data->solver[DATA][z])
+                {
+                    if (data->solver[DATA][z] == data->rules[i][j])
+                    {
+                        break ;
+                    }
+                    z++;
+                }
+                if ('\0' == data->solver[DATA][z])
+                {
+                    data->solver[DATA][z] = data->rules[i][j];
+                    data->solver[STATUS][z] = '-';//FALSE;
+                    data->solver[FACTS][z] = '-';//FALSE;
+                }
+            }
+            j++;
+        }
+        i++;
     }
-    //add to solver ruls leters
-    (void)j;
 }
 
 void algorithm(t_expert *data)
 {
     add_to_solver(data);
-    printf("data->facts[i] = %s\n", data->solver[DATA]);
-    (void)data;
+    ft_printf("  [DATA] = %s\n", data->solver[DATA]);
+    ft_printf("[STATUS] = %s\n", data->solver[STATUS]);
+    ft_printf(" [FACTS] = %s\n", data->solver[FACTS]);
 }
